26c76ae5002fec6fc380ab993b04eb7a8b6e11b4
Andrei-Mihai Nicolae <contact@anicolae.ro>
2021-12-09 22:25:00 +0200

    complete day1 part 1

--- src/adventofcode/day1.clj
+++ src/adventofcode/day1.clj
@@ -1,14 +1,16 @@
 (ns adventofcode.day1
   (:require [clojure.java.io :as io]))
 
-(defn part-1 []
+(defn measurements []
   (let [xs (with-open [rdr (io/reader (io/file "data/day-1.txt"))]
-    (->> (line-seq rdr)
-      (mapv #(Long/parseLong %))
-         (println)))]))
+             (->> (line-seq rdr)
+                  (mapv #(Long/parseLong %))))]
+    xs))
 
-(part-1)
+(defn number-of-increases [m]
+  (let [[x & xs] m]
+    (if (empty? xs)
+      0
+      (+ (number-of-increases xs) (if (< x (first xs)) 1 0)))))
 
-(comment
-  (part-1)
-  )
+(number-of-increases (measurements))
