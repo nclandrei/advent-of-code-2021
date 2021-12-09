29c55e5ba2175da28e8790ab4be003e28928aa62
Andrei-Mihai Nicolae <contact@anicolae.ro>
2021-12-10 01:33:54 +0200

    complete day 2 part 2

--- src/adventofcode/day2.clj
+++ src/adventofcode/day2.clj
@@ -12,15 +12,27 @@
 (defn transform-pair [l]
   (vector (keyword (first l)) (Integer/parseInt (second l))))
 
-(defn update-positions [m el]
+(defn update-positions-1 [m el]
   (cond
     (= (first el) :forward) (assoc m :horizontal (+ (get m :horizontal) (second el)))
     (= (first el) :down) (assoc m :vertical (+ (get m :vertical) (second el)))
     (= (first el) :up) (assoc m :vertical (- (get m :vertical) (second el)))))
 
-(defn final-position [l]
-  (reduce #(update-positions %1 %2) {:horizontal 0 :vertical 0} l))
+(defn update-positions-2 [m el]
+  (cond
+    (= (first el) :forward) (assoc m :horizontal (+ (get m :horizontal) (second el))
+                                     :vertical (+ (get m :vertical) (* (get m :aim) (second el))))
+    (= (first el) :down) (assoc m :aim (+ (get m :aim) (second el)))
+    (= (first el) :up) (assoc m :aim (- (get m :aim) (second el)))))
+
+(defn final-position [l v]
+  (let [fun (if (= v 1) update-positions-1 update-positions-2)]
+    (reduce #(fun %1 %2) {:horizontal 0 :vertical 0 :aim 0} l)))
 
 (defn part-1 []
-  (let [data (final-position (map #(transform-pair %) (transform-data)))]
+  (let [data (final-position (map #(transform-pair %) (transform-data)) 1)]
+    (* (get data :horizontal) (get data :vertical))))
+
+(defn part-2 []
+  (let [data (final-position (map #(transform-pair %) (transform-data)) 2)]
     (* (get data :horizontal) (get data :vertical))))
